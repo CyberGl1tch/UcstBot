0c080e208d6c742f4cc08cf701e9d061d101fc8d
rounnus <rounnus@gmail.com>
2019-12-28 17:01:06 +0200

    upgrade

--- utils/API.js
+++ utils/API.js
@@ -20,25 +20,56 @@ function httpGet(URL)
 		return false;
 	}
 }
+function informationsAboutCTF(ctfInfos,json,infoToShow){
+	/*
+		This function get's
+		1) One list that is the information we want from the API
+		2) The json that have the information we need.
+		3) how many things to show.
+
+	*/
+	let informationCTFList=ctfInfos; // The informations
+	let jsonFormat=json; // the json
+	let infoCTFMap={}; // create an empty map variable
+	let show=infoToShow;
+	// getting data
+	for (let aboutCTF=0;aboutCTF<show;aboutCTF++){
+			// Getting one by one all the informations
+			for (let info=0;info<informationCTFList.length;info++){
+				// We add  all the informations about the CTF's to map variable
+				infoCTFMap[`${informationCTFList[info]}_${aboutCTF}`]=jsonFormat[aboutCTF][informationCTFList[info]];
+			}
+	}
+	return infoCTFMap; // returns the map variable
+}
 function getUpcommingCTF(){
 	/* 
-		This function get's 
+		This function returns
 		the informations about	
 		the upcoming CTFs events.
 		The information is given by the API: https://ctftime.org/api/
 	*/
 	let upCTFinfo=['start','finish','duration','title','logo','format','id','participants','description']; // These are the informations we want from the API.
 	let getUpcomming=httpGet("https://ctftime.org/api/v1/events/?limit=5&start="+nowTimeStamp+"");
-	let jsonFormat=JSON.parse((getUpcomming.replace(/<b[^>]*>/g, '**')).replace(/<i[^>]*>/g, '__')); // make the json.	
-	let upCTFMapInfo={};
-	let eventsToShow=5;
-	// Getting data.
-	for (let event=0;event<eventsToShow;event++){
-		// Getting one by one all the informations
-		for (let info=0;info<upCTFinfo.length;info++){
-			// We create a map that have all the informations about the CTF's.
-			upCTFMapInfo[`${upCTFinfo[info]}_${event}`]=jsonFormat[event][upCTFinfo[info]];
-	 	}
-	}
-	return upCTFMapInfo;
+	let jsonFormat=JSON.parse((getUpcomming.replace(/<b[^>]*>/g, '')).replace(/<i[^>]*>/g, '__')); // make the json.	
+	let eventsToShow=5; // how many events to show
+	let upMap=informationsAboutCTF(upCTFinfo,jsonFormat,eventsToShow); // call the function to get the informations about CTF events
+	console.log(upMap);
+}
+function topCTFTeams(year){
+	/*
+		This Function takes
+		year and returns
+		the top CTF teams for 
+		the input year.
+		The information is given by the API: https://ctftime.org/api/
+	*/
+	let teamInfos=['team_name','points','team_id'];
+	let getTopTeams=httpGet("https://ctftime.org/api/v1/top/"+year+"/");
+	let jsonFormat=JSON.parse(getTopTeams); // make the json.
+	let ctfYear=jsonFormat[year];
+	let teamToShow=10;
+	let mapOfCTF=informationsAboutCTF(teamInfos,ctfYear,teamToShow);
+	console.log(mapOfCTF);
+	return mapOfCTF;
 }
