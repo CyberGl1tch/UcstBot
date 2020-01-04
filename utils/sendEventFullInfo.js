2987c3f856ac32f783c6e98b41c80a01ccfd5714
magas8 <komavrellis@gmail.com>
2020-01-04 22:55:12 +0200

    Module fix

--- utils/sendEventFullInfo.js
+++ utils/sendEventFullInfo.js
@@ -1,11 +1,8 @@
-
-
-
 const Discord = require("discord.js");
 const botconfig = require("../configs/botconfig.json");
 const ctf = require("../utils/API.js");
 const time = require("../utils/time.js");
-const toemojis = require(`../utils/toemojis.js`)
+const toemojis = require(`./toemojis.js`)
 
 
 
